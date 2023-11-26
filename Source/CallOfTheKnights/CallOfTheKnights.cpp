f314f43746883a71c66fcc7234bb934aa3d79a15
AlvaroBishop <bishopgamma@gmail.com>
2023-11-26 00:38:38 -0600

    add finished Shooter Mechanics

---
+++ Source/CallOfTheKnights/CallOfTheKnights.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CallOfTheKnights.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CallOfTheKnights, "CallOfTheKnights" );
---
+++ Source/CallOfTheKnights/CallOfTheKnightsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CallOfTheKnightsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALLOFTHEKNIGHTS_API ACallOfTheKnightsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
